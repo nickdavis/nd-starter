ea3dbb8c9a644f979b2f27d81debc20ea419fb6d
Nick Davis <nick@esanctuary.ch>
2016-11-01 10:21:39 +0100

    Update global constants
    Remove unneeded config global constant
    
    (Alain advises only have two global constants, one for directory and one for URL but I’m going to keep in version and name for now)

--- functions.php
+++ functions.php
@@ -20,11 +20,10 @@ namespace NickDavis\Starter;
 function init_constants() {
 	$child_theme = wp_get_theme();
 
+	define( 'CHILD_THEME_DIR', get_stylesheet_directory() );
 	define( 'CHILD_THEME_NAME', $child_theme->get( 'Name' ) );
 	define( 'CHILD_THEME_URL', $child_theme->get( 'ThemeURI' ) );
 	define( 'CHILD_THEME_VERSION', $child_theme->get( 'Version' ) );
-	define( 'CHILD_THEME_DIR', get_stylesheet_directory() );
-	define( 'CHILD_CONFIG_DIR', CHILD_THEME_DIR . '/config/' );
 }
 
 init_constants();
